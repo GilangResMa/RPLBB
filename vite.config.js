dbec5ad23fae646352956cf790c569703a810d2a
Simbah_Ngoding <gilangresma02@gmail.com>
2025-06-18 14:26:23 +0700

    ubah dikit

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,20 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
+                tailwindcss(),
                 "resources/css/app.css",
-                "resources/css/detailkamar.css",
-                "resources/css/payment.css",
+                "public/css/detailkamar.css",
+                "public/css/payment.css",
                 "resources/js/app.js",
             ],
             refresh: true,
         }),
     ],
 });
+
+
